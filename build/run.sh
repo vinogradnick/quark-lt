27bf0e4dcbdb4d3496052821c1be818db59a3120
vinogradovnick <vinogradovnick32@gmail.com>
2020-04-23 01:57:45 +0500

    update ignore and add start files

--- build/run.sh
+++ build/run.sh
@@ -1,1 +1,17 @@
-make -l
+cgo=0
+arch=amd64
+CD windows
+GOOS=windows GOARCH=$arch CGO_ENABLED=$cgo go build ../../cmd/quark-apiserver/quark-apiserver.go
+GOOS=windows GOARCH=$arch CGO_ENABLED=$cgo go build ../../cmd/quark-node/quark-node.go
+GOOS=windows GOARCH=$arch CGO_ENABLED=$cgo go build ../../cmd/quark-worker/quark_worker.go
+cd ..
+# shellcheck disable=SC2164
+cd unix
+GOOS=linux GOARCH=$arch CGO_ENABLED=$cgo go build ../../cmd/quark-apiserver/quark-apiserver.go
+GOOS=linux GOARCH=$arch CGO_ENABLED=$cgo go build ../../cmd/quark-node/quark-node.go
+GOOS=linux GOARCH=$arch CGO_ENABLED=$cgo go build ../../cmd/quark-worker/quark_worker.go
+CD ..
+
+tar -cvf apiserver.tar.gz ./unix/quark-apiserver ./config/core.yaml ./client
+tar -cvf node.tar.gz ./unix/quark-node ./unix/quark_worker ./config/node.yaml
+tar -cvf node.tar.gz ./unix/quark_worker ./config/worker.yaml
--- build/start.bat
+++ build/start.bat
@@ -1,3 +1,12 @@
-go build ..\cmd\quark-apiserver\*.go
-go build ..\cmd\quark-node\*.go
-go build ..\cmd\quark-worker\*.go
+set cgo=0
+set arch=amd64
+CD windows
+env GOOS=windows GOARCH=%arch% CGO_ENABLED=%cgo% go build ..\..\cmd\quark-apiserver\quark-apiserver.go
+env GOOS=windows GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-node\quark-node.go
+env GOOS=windows GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-worker\quark_worker.go
+CD ..
+CD unix
+env GOOS=linux GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-apiserver\quark-apiserver.go
+env GOOS=linux GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-node\quark-node.go
+env GOOS=linux GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-worker\quark_worker.go
+CD ..
