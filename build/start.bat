d6cb8a777eaf83881b275e72464420732c339d9b
vinogradovnick <vinogradovnick32@gmail.com>
2020-04-23 13:05:37 +0500

    update ignore

--- build/start.bat
+++ build/start.bat
@@ -1,12 +1,4 @@
-set cgo=0
-set arch=amd64
-CD windows
-env GOOS=windows GOARCH=%arch% CGO_ENABLED=%cgo% go build ..\..\cmd\quark-apiserver\quark-apiserver.go
-env GOOS=windows GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-node\quark-node.go
-env GOOS=windows GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-worker\quark_worker.go
-CD ..
-CD unix
-env GOOS=linux GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-apiserver\quark-apiserver.go
-env GOOS=linux GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-node\quark-node.go
-env GOOS=linux GOARCH=%arch% CGO_ENABLED=%cgo%  go build ..\..\cmd\quark-worker\quark_worker.go
-CD ..
+go build ..\cmd\quark-apiserver\quark-apiserver.go
+go build ..\cmd\quark-node\quark-node.go
+go build ..\cmd\quark-worker\quark_worker.go
+
